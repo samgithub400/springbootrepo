629c1b5cbb311479f2da604bbbbc29f7a36a205a
Tohit Shaikh <tohit.shaikh@gspann.com>
2022-11-11 12:54:55 +0530

    First commit

---
+++ springboot-with-AWS/src/main/java/com/ios/SpringbootWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.ios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAwsApplication.class, args);
+	}
+
+}
