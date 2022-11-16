1ec6e4906a5c87854fda4fe3dc08fcd0db7169fe
samgithub400 <shaikhtohit95@gmail.com>
2022-11-16 13:25:40 +0530

    UserNotFoundException Added in Api

--- springboot-with-AWS/src/main/java/com/ios/model/User.java
+++ springboot-with-AWS/src/main/java/com/ios/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -18,7 +19,7 @@ import lombok.ToString;
 @Entity
 @Table(name = "UserInfo")
 public class User {
-
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "UserId")
 	private long userId;
