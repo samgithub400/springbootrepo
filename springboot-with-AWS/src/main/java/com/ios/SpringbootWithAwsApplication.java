fe3b0c7c8c0166d5632cc50d4f3749bea545f1f0
samgithub400 <shaikhtohit95@gmail.com>
2022-11-16 12:59:02 +0530

    Log added in Main class

--- springboot-with-AWS/src/main/java/com/ios/SpringbootWithAwsApplication.java
+++ springboot-with-AWS/src/main/java/com/ios/SpringbootWithAwsApplication.java
@@ -4,13 +4,16 @@ package com.ios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringbootWithAwsApplication {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome TO The Spring Boot App With AWS..!");
-		System.out.println("Welcome Back....!");
+		log.info("Spring Boot With AWS App Started...!");
 		SpringApplication.run(SpringbootWithAwsApplication.class, args);
 	}
 
